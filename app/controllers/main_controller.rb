c7c10ef18fda2e7e995fe0430aca8d528b4b9583
Regine Regino <rregino@degica.com>
2022-07-20 10:38:53 +0800

    Add flash

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+    # from ApplicationController
+    flash[:notice] = "Logged in successfully"
+    flash[:alert] = "Invalid email or password"
   end
 end
