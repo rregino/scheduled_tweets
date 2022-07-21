3601dfa5293f439099f85b544bbb97ad0965438b
Regine Regino <rregino@degica.com>
2022-07-21 16:03:47 +0800

    Part 25 Added session validation for connecting twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,30 @@
+# Note: going to /auth/twitter in the browser won't work anymore, just click the button instead
 class OmniauthCallbacksController < ApplicationController
+  before_action :require_user_logged_in!
+
   def twitter
-    render plain: "Success"
+    # Rails.logger.info auth
+    Rails.logger.info auth.info
+
+    # Need to have Elevated access in twitter project to access the name/username/image
+    # Query the db -
+    # Do you have a record with this user name?
+    # If present, give the first one
+    # else give a new twitter account with that username assigned
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+    twitter_account.update(
+      name: auth.info.name,
+      username: auth.info.nickname,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    )
+
+    redirect_to root_path, notice: "Successfully connected your account"
   end
+
+  def auth
+    request.env['omniauth.auth'] #convenience method
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 
 
 class User < ApplicationRecord
+  has_many :twitter_accounts
   has_secure_password #
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
