2db4a494b35e2c5ce6421914ed739342dba6afed
Regine Regino <rregino@degica.com>
2022-07-25 12:30:10 +0800

    Test gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "scheduled-tweetz.herokuapp.com" }
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.default_url_options = { host: "https://scheduled-tweetz.herokuapp.com" }
+  # config.action_mailer.perform_deliveries = false
 
   config.active_job.queue_adapter = :sidekiq
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            ENV["GMAIL_ACCOUNT"],
+    password:             ENV["GMAIL_AUTH"],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
+
 end
